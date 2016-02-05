ed5af7ccf485fa25c27ec5f47959ae4ab849ca23
Anshit Srivastava <anshit92@live.com>
2016-02-05 11:54:35 +0530

    Initial Comment

---
+++ src/com/mindtree/jdbc/JdbcConnection.java
@@ -0,0 +1,38 @@
+package com.mindtree.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class JdbcConnection {
+  
+	public static Connection connectionJdbc;
+	
+	static Connection makeSFObject() throws ClassNotFoundException, SQLException
+	{
+     Class.forName("com.mysql.jdbc.Driver");
+      connectionJdbc = DriverManager.getConnection("jdbc:mysql://localhost:3306/employee_order_db?user=root&password=Welcome123");
+     return connectionJdbc;
+   }
+	
+	public static Connection getConnectionObject() throws ClassNotFoundException, SQLException
+	{
+		if(connectionJdbc == null){
+			connectionJdbc = makeSFObject();
+		}
+		return connectionJdbc;
+	}
+	
+	public static void closeConn(Connection conn)
+	{
+		if(conn!=null)
+		{
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+}
---
+++ src/com/mindtree/service/Service1.java
@@ -0,0 +1,105 @@
+package com.mindtree.service;
+
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.json.simple.JSONObject;
+
+import com.mindtree.jdbc.JdbcConnection;
+
+@Path("/")
+public class Service1 {
+	
+	public Connection connec;
+	public Service1()
+	{}
+
+	@SuppressWarnings("unchecked")
+	@GET
+	@Path("/display/{table}/{name}")
+	@Produces(MediaType.TEXT_PLAIN)
+	public Response display(InputStream incomingData,@PathParam("name") String name,@PathParam("table") String table)  {
+		JSONObject jsonObject = new JSONObject();
+		try {
+	    connec=JdbcConnection.getConnectionObject();
+		Statement stmt=connec.createStatement();
+		String query = "select * from "+table+" where name='"+name+"'";
+	    ResultSet rs =  stmt.executeQuery(query);
+	    ResultSetMetaData rsmd = rs.getMetaData();
+	    int columnsNumber = rsmd.getColumnCount();
+	    
+	    while (rs.next()) {
+	        for (int i = 1;i <= columnsNumber; i++) {
+	            String columnValue = rs.getString(i);
+	            System.out.println(columnValue);
+	            jsonObject.put(rsmd.getColumnName(i), columnValue);
+	        }
+	    }} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return Response.status(200).entity(""+jsonObject.toString()).build(); 
+	}
+	
+	@GET
+	@Path("/insertEmp/{name}/{age}/{gender}")
+	@Produces(MediaType.TEXT_PLAIN)
+	public Response insertEmp(InputStream incomingData,@PathParam("name") String name,@PathParam("age") int age,@PathParam("gender") String gender)  {
+		try {
+	    connec=JdbcConnection.getConnectionObject();
+		Statement stmt=connec.createStatement();
+		String query = "insert into employee values('"+name+"',"+age+",'"+gender+"')";
+		stmt.executeUpdate(query);
+	  } catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return Response.status(200).build();
+	}
+	
+	@GET
+	@Path("/insertOrder/{name}/{description}")
+	@Produces(MediaType.TEXT_PLAIN)
+	public Response insertOrder(InputStream incomingData,@PathParam("name") String name,@PathParam("description") String description)  {
+		try {
+	    connec=JdbcConnection.getConnectionObject();
+		Statement stmt=connec.createStatement();
+		String query = "insert into orders values('"+name+"','"+description+"')";
+		stmt.executeUpdate(query);
+	  } catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return Response.status(200).build();
+	}
+	
+	
+	@GET
+	@Path("/hello")
+	@Produces(MediaType.TEXT_PLAIN)
+	public Response displayS(InputStream incomingData) throws ClassNotFoundException, SQLException {
+		String str="Hello World";
+		return Response.status(200).entity(""+str).build();
+	}
+
+}
